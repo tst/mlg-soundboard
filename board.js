005ce589cd3903e359e58ab69965bfb58065f533
tst <tst@panictank.net>
2014-05-16 19:39:13 +0200

    cleaned board.js up a bit

--- board.js
+++ board.js
@@ -1,62 +1,11 @@
-
+// init global vars
 window.onload = init;
 var context;
 var bufferLoader;
-
-/* buffer loader class */
-
-function BufferLoader(context, urlList, callback) {
-  this.context = context;
-  this.urlList = urlList;
-  this.onload = callback;
-  this.bufferList = new Array();
-  this.loadCount = 0;
-}
-
-BufferLoader.prototype.loadBuffer = function(url, index) {
-  // Load buffer asynchronously
-  var request = new XMLHttpRequest();
-  request.open("GET", url, true);
-  request.responseType = "arraybuffer";
-
-  var loader = this;
-
-  request.onload = function() {
-    // Asynchronously decode the audio file data in request.response
-    loader.context.decodeAudioData(
-      request.response,
-      function(buffer) {
-        if (!buffer) {
-          alert('error decoding file data: ' + url);
-          return;
-        }
-        loader.bufferList[index] = buffer;
-        if (++loader.loadCount == loader.urlList.length)
-          loader.onload(loader.bufferList);
-      },
-      function(error) {
-        console.error('decodeAudioData error', error);
-      }
-    );
-  }
-
-  request.onerror = function(e) {
-    alert('BufferLoader: XHR error');
-    console.log(e);
-  }
-
-  request.send();
-}
-
-BufferLoader.prototype.load = function() {
-  for (var i = 0; i < this.urlList.length; ++i)
-      this.loadBuffer(this.urlList[i], i);
-}
-
-/* end */
+var bList;
 
 
-
+// mp3 files and sources
 var soundfiles = new Array("2SAD4ME.mp3",
                            "AIRHORN.mp3",
                            "Darude - Dankstorm.mp3",
@@ -107,21 +56,84 @@ var sourcefiles = new Array("https://www.youtube.com/watch?v=JSnR80kY0m0",
 
 
 
+
+
+
+
+// buffer loader class
+
+function BufferLoader(context, urlList, callback) {
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = new Array();
+  this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function(url, index) {
+  // Load buffer asynchronously
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  var loader = this;
+
+  request.onload = function() {
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+      request.response,
+      function(buffer) {
+        if (!buffer) {
+          alert('error decoding file data: ' + url);
+          return;
+        }
+        loader.bufferList[index] = buffer;
+        if (++loader.loadCount == loader.urlList.length)
+          loader.onload(loader.bufferList);
+      },
+      function(error) {
+        console.error('decodeAudioData error', error);
+      }
+    );
+  }
+
+  request.onerror = function(e) {
+    alert('BufferLoader: XHR error');
+    console.log(e);
+  }
+
+  request.send();
+}
+
+BufferLoader.prototype.load = function() {
+  for (var i = 0; i < this.urlList.length; ++i)
+      this.loadBuffer(this.urlList[i], i);
+}
+
+// END bufferloader class
+
+
+
+
 function init() {
     // sources and download links
     $("#sourcelinks").append("<ul class=\"list-group\">");
     var sourcelinks = $("#sourcelinks ul");
+
     $.each(soundfiles, function (i) {
-        sourcelinks.append("<li class=\"list-group-item\"><a href=\"" + sourcefiles[i] + "\" target=\"_blank\">" + soundfiles[i].slice(0, -4) + "</a></li>")
+        sourcelinks.append("<li class=\"list-group-item\"><a href=\"" 
+                           + sourcefiles[i] + "\" target=\"_blank\">" + 
+                           soundfiles[i].slice(0, -4) + "</a></li>")
     });
     
     $("#downloadlinks").append("<ul class=\"list-group\">");
     var downloadlinks = $("#downloadlinks ul");
     $.each(soundfiles, function (i) {
-        downloadlinks.append("<li class=\"list-group-item\"><a href=\"http://soundboard.panictank.net/" + soundfiles[i] + "\">" + soundfiles[i] + "</a></li>")
+        downloadlinks.append("<li class=\"list-group-item\"><a href=\"http://soundboard.panictank.net/" 
+                             + soundfiles[i] + "\">" + soundfiles[i] + "</a></li>")
     });
 
-    // soundboard stuff
+    // load mp3 files into RAM and active finishedLoading()
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     context = new AudioContext();
 
@@ -134,15 +146,14 @@ function init() {
     bufferLoader.load();
 }
 
-var bList;
 
 function finishedLoading(bufferList) {
-    var bList = bufferList;
+    bList = bufferList;
     
     $("#loading").hide();
-    /* add buttons */
-    for (var i = 0; i < soundfiles.length; ++i)
-    {
+
+    // add buttons
+    $.each(soundfiles, function (i) {
         var btn = document.createElement("input");
         btn.type = "button";
         btn.value = soundfiles[i].slice(0,-4);
@@ -151,14 +162,17 @@ function finishedLoading(bufferList) {
         btn.classList.add("btn", "btn-primary", "btn-lg");
 
         document.getElementById("buttons").appendChild(btn);
-    }
+    });
 
 
-    /* add keyboard controls 48 */
+    // add keyboard controls
     $(document).keypress(function (e) {
+        // for 0 to 9
         if (e.which >= 48 && e.which <= 57) {
             $("#" + (e.which - 48)).click();
-        } else if (e.which >= 97 && e.which <= 122)
+        }
+        // for a to z
+        else if (e.which >= 97 && e.which <= 122)
         {
             $("#" + (e.which - 87)).click();
         }
@@ -179,15 +193,16 @@ function play(buffer, drive, gain) {
         gainN.connect(context.destination); 
 
     } else {
-
+        // workaround for using overdrive which is a bit low in volume
         var overdrive = new Overdrive(context);
         overdrive.drive = drive;
         overdrive.color = 8000;
         gainN.connect(overdrive.input);
-
+    
         var gain2 = context.createGain();
         overdrive.connect(gain2);
-
+        
+        // apply second gain of 2.6
         gain2.gain.value = 2.6;
         gain2.connect(context.destination);
     }
